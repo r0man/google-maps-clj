e06b64c9b1ce0d4c7590c924f28df5254e928cad
Roman Scherer <roman.scherer@burningswell.com>
2010-02-18 17:15:42 +0100

    initial import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject google-maps-clj "0.0.1-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.1.0-alpha-SNAPSHOT"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[autodoc "0.7.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]
+                     [swank-clojure "1.1.0"]])
