99e9e195d7ae243ea00e7be23e4670ae6bd117fa
Roman Scherer <roman.scherer@burningswell.com>
2010-02-19 16:04:39 +0100

    updated project file

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject google-maps "0.0.1-SNAPSHOT"
+  :author "Roman Scherer"
   :description "Google Maps Clojure Library"
+  :url "http://github.com/r0man/google-maps-clj"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[autodoc "0.7.0"]
