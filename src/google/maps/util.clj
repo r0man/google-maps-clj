94866965adfe4fe87e40550e734c8e6d152b7d01
Roman Scherer <roman.scherer@burningswell.com>
2010-02-19 15:54:54 +0100

    added doc string

--- src/google/maps/util.clj
+++ src/google/maps/util.clj
@@ -2,7 +2,9 @@
   (:import java.net.URLEncoder)
   (:use [clojure.contrib.str-utils2 :only (join)]))
 
-(defmulti url-encode (fn [obj & [encoding]] (class obj)))
+(defmulti url-encode
+  "Returns a url encoded string of the given object."
+  (fn [obj & [encoding]] (class obj)))
 
 (defmethod url-encode String [string & [encoding]]
   (. URLEncoder encode string (or encoding "UTF-8")))
