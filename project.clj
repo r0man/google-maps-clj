9250cf046566384383fdb72d6a5625d621ed05d7
Roman Scherer <roman@soundcloud.com>
2010-06-02 19:15:53 +0200

    no autodoc

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :url "http://github.com/r0man/google-maps-clj"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]]
-  :dev-dependencies [[autodoc "0.7.0"]
+  :dev-dependencies [;[autodoc "0.7.1"]
                      [lein-clojars "0.5.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
