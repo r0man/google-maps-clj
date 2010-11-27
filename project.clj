fea2842705aeaf1246686acfd5ecd4e88122b71a
Roman Scherer <roman@burningswell.com>
2010-11-27 17:00:14 +0100

    Bumbed patch version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject google-maps "0.4-SNAPSHOT"
+(defproject google-maps "0.4.1-SNAPSHOT"
   :author "Roman Scherer"
   :description "Google Maps Clojure Library"
   :url "http://github.com/r0man/google-maps-clj"
