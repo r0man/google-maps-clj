3668c74106e1410f104df9637f0dfb20e1705f3a
Roman <roman@burningswell.com>
2011-01-28 12:18:52 +0100

    Autodoc upgrade.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :url "http://github.com/r0man/google-maps-clj"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[autodoc "0.7.1"]
+  :dev-dependencies [[org.clojars.rayne/autodoc "0.8.0-SNAPSHOT"]
                      [swank-clojure "1.2.1"]])
