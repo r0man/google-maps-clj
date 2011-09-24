0808451cf1ce48ffcdbcd5c341e82c80f7187b3d
Roman <roman@burningswell.com>
2011-09-24 11:22:18 +0200

    Release 0.5.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject google-maps "0.5.0-SNAPSHOT"
+(defproject google-maps "0.5.0"
   :author "Roman Scherer"
   :description "Google Maps Clojure Library"
   :url "http://github.com/r0man/google-maps-clj"
   :dependencies [[clj-http "0.2.0"]
-                 [org.clojure/clojure "1.3.0-RC0"]
+                 [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]]
   :multi-deps {"1.2.1" [[clj-http "0.2.0"]
                         [org.clojure/clojure "1.2.1"]
