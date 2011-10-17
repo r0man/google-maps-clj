8f869979983b48142588ae2cefdacc61b60a65f8
Roman <roman@burningswell.com>
2011-10-17 09:15:40 +0200

    Upgraded data.json and clj-http.

--- project.clj
+++ project.clj
@@ -2,9 +2,6 @@
   :author "Roman Scherer"
   :description "Google Maps Clojure Library"
   :url "http://github.com/r0man/google-maps-clj"
-  :dependencies [[clj-http "0.2.0"]
+  :dependencies [[clj-http "0.2.1"]
                  [org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]]
-  :multi-deps {"1.2.1" [[clj-http "0.2.0"]
-                        [org.clojure/clojure "1.2.1"]
-                        [org.clojure/data.json "0.1.1"]]})
+                 [org.clojure/data.json "0.1.2"]])
