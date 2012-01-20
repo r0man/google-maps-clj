f34ddba379da31681a63a90f8a91e6f26371ce58
Roman <roman@burningswell.com>
2012-01-20 17:58:03 +0100

    Switched to clj-http 0.2.7.

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :author "Roman Scherer"
   :description "Google Maps Clojure Library"
   :url "http://github.com/r0man/google-maps-clj"
-  :dependencies [[clj-http "0.2.6"]
+  :dependencies [[clj-http "0.2.7"]
                  [org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]])
