29efb3c6309bb7a3121ccdf0277cfc44cdbb5a4f
Roman <roman@burningswell.com>
2012-04-16 20:01:50 +0200

    Switched to Clojure 1.4.0.

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "http://github.com/r0man/google-maps-clj"
   :author "Roman Scherer"
   :dependencies [[clj-http "0.2.7"]
-                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]]
   :min-lein-version "2.0.0")
